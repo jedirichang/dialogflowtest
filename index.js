fa5a77e39d26733ebc70aeb9235a6eca04ef7a0b
Sharma Richang <richang.sharma@dieboldnixdorf.com>
2019-09-11 12:42:46 +0530

    body parser added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
-
+const bodyparser=require('body-parser');
+app.use(bodyparser.json());
 app.post('/getMovies',(req,res)=>{
     console.log(req.body,req.query,req.params);
     res.send({'test':'test'});
