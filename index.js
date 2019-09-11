6ac0530422073e9df374f81802d889bb5f9739b2
Sharma Richang <richang.sharma@dieboldnixdorf.com>
2019-09-11 12:26:02 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 
 app.post('/getMovies',(req,res)=>{
-    console.log(req);
+    console.log(req.body,req.query,req.params);
     res.send({'test':'test'});
 });
 
